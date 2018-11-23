9bf80570a789fcd38a04a76872c97f3405378663
Jose Roberto Leite da Silva Filho <jose.leite@SLT-002200.solutis.net.br>
2018-11-23 19:08:43 -0300

    lista de generos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
+
 function pesquisar(){
- 
-    let xmlhttp = new XMLHttpRequest();
-    let url = "https://leandrojsa.github.io/movies.html";
-    let entrada_ano = document.querySelector("#ano").value;
-    let entrada_atores = document.querySelector("#atores").value;
-    let entrada_info = document.querySelector("#outros").value;
 
-    xmlhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let responseText = this.responseText;
+    
 
-            const parser = new DOMParser();
-
-            const xmlDoc= parser.parseFromString(responseText, 'text/html');
-            if(xmlDoc){
-
-            }
-        }
-    }
 }
+
+
+function limparForm(){
+
+    let ano_entrada = document.getElementById("ano").value;
+    let genero_entrada = document.getElementById("genero").value;
+    let ator_entrada = document.getElementById("ator").value;
+    let outros_entrada = document.getElementById("outros").value;
+
+    console.log(ano_entrada);
+
+}
+
+
+
